51ed294b9cd0345cc53b22d80a75ccc996a82a9c
milankmr <milankmr@gmail.com>
2018-05-20 14:08:25 +0530

    Add get Todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 //middleware
@@ -31,6 +33,30 @@ app.get('/todos',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id',(req,res)=>{
+  //res.send(req.params);
+  var id = req.params.id;
+
+  //validatee ID using isValid
+  if(!ObjectID.isValid(id)){
+    //console.log('ID not valid');
+    return res.status(404).send();
+  }
+  else {
+    Todo.findById({_id:id}).then((todo)=>{
+      if(todo)
+      {
+      res.send(todo);
+    }else{
+      res.status(404).send();
+    }
+    },(e)=>{
+      res.send(404).send();
+    });
+  }
+
 });
 
 app.listen(3000,()=>{
