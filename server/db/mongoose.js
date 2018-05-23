2bfe90061564eebf43e227718ecd2b90bbab9588
milankmr <milankmr@gmail.com>
2018-05-23 21:27:03 +0530

    Patch done

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-// mongoose.connect("mongodb://milan:Sasken%401989@ds115712.mlab.com:15712/milandb").then((err,db)=>{
-//   console.log("connected to mlab database server");
-// },(err)=>{
-//   if(err){
-//     return console.log("err in connecting",err);
-//   }
-// }).catch((e)=>{
-//   console.log("Exception in connection",e);
-// });
+mongoose.connect("mongodb://milan:Sasken%401989@ds115712.mlab.com:15712/milandb").then((err,db)=>{
+  console.log("connected to mlab database server");
+},(err)=>{
+  if(err){
+    return console.log("err in connecting",err);
+  }
+}).catch((e)=>{
+  console.log("Exception in connection",e);
+});
 
-mongoose.connect("mongodb://localhost/TodoApp");
+//mongoose.connect("mongodb://localhost/TodoApp");
 
 module.exports = {mongoose};
